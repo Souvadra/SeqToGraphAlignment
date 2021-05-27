99507092fa9e941ce613f6e7306a8c849cc0ffec
Souvadra <souvadra01@gmail.com>
2021-05-27 10:54:16 +0530

    added argument parser for ease of use

--- minigraph_operations/synthetic_MT.py
+++ minigraph_operations/synthetic_MT.py
@@ -1,4 +1,5 @@
 from random import randint
+import argparse
 
 # A Global dictionary 
 alphabet = dict()
@@ -26,20 +27,61 @@ def randomlines(line,index):
         else:
             return line
 
-# Read the MT-human file
-fR = open("MT-human.fa",'r')
-mt_human = []
-for line in fR:
-    mt_human.append(line)
+def argument_parsing():
+    info = """
+        making the command line arguments friendlier.
+    """
+    parser = argparse.ArgumentParser(description=info)
+    parser.add_argument(
+        "-infile",
+        "--input_file_loc",
+        type=str,
+        required=True,
+        help="directory of the input file"
+    )
+    parser.add_argument(
+        "-outdir",
+        "--output_directory",
+        type=str,
+        required=True,
+        help="Enter the directory of the output file(s)"
+    )
+    parser.add_argument(
+        "-num_files",
+        "--number_of_files",
+        type=int,
+        required=True,
+        help="Enter the number of synthetic fasta files to build"
+    )
+    parser.add_argument(
+        "-outfile",
+        "--output_file",
+        type=str,
+        required=True,
+        help="Name of the output files, e.g. MT-synthetic"
+    )
 
-num_examples = 2
+    return parser.parse_args()
 
-for i in range(num_examples):
-    name = "MT-synthetic" + str(i) + ".fa"
-    fW = open(name,'w')
-    for line in mt_human:
-        fW.writelines(randomlines(line,i))
-    fW.close()
+if __name__ =="__main__":
+    args = argument_parsing()
+
+    num_examples = args.number_of_files
+    input_file = args.input_file_loc
+    out_dir = args.output_directory
+    out_file = args.output_file
+
+    # Read the MT-human file
+    fR = open(input_file,'r')
+    input_fasta = []
+    for line in fR:
+        input_fasta.append(line)
+
+    # construct the output files 
+    for i in range(num_examples):
+        name = out_dir + out_file + str(i) + ".fa"
+        fW = open(name,'w')
+        for line in input_fasta:
+            fW.writelines(randomlines(line,i))
+        fW.close()
     
-
-
